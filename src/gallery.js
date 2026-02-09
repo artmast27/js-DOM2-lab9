8a0df7bcd3399d6f64f5be6e519fbc197f5f3917
artmast27 <artemko4819@gmail.com>
2026-02-09 12:03:37 +0100

    Update gallery.js

--- src/gallery.js
+++ src/gallery.js
@@ -1,1 +1,27 @@
-// Gallery logic
+const gallery = document.getElementById("gallery");
+
+export function renderImages(images) {
+  images.forEach(img => {
+    const image = document.createElement("img");
+    image.src = img.download_url;
+    image.alt = img.author;
+    image.dataset.id = img.id;
+    gallery.appendChild(image);
+  });
+}
+
+export function clearGallery() {
+  gallery.innerHTML = "";
+}
+
+export function removeLastImage() {
+  if (gallery.lastChild) {
+    gallery.removeChild(gallery.lastChild);
+  }
+}
+
+export function reverseGallery() {
+  const images = Array.from(gallery.children);
+  gallery.innerHTML = "";
+  images.reverse().forEach(img => gallery.appendChild(img));
+}
