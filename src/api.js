84c5ba90ebb1e86f293fb531102ed9b947c951e5
artmast27 <artemko4819@gmail.com>
2026-02-09 12:03:07 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,1 +1,20 @@
-// API logic
+const API_URL = "https://picsum.photos/v2/list";
+
+// завантаження унікальних фото
+export async function fetchImages(limit = 4, usedIds) {
+  const response = await fetch(`${API_URL}?limit=100`);
+  const data = await response.json();
+
+  const result = [];
+
+  for (const img of data) {
+    if (!usedIds.has(img.id)) {
+      usedIds.add(img.id);
+      result.push(img);
+    }
+    if (result.length === limit) break;
+  }
+
+  return result;
+}
+
